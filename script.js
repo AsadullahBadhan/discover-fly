f3cb7c90e753dd801b21def585c3c12306412cf1
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-09-23 01:30:46 +0600

    ticket count function update for all class

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-const firstClassPlusBtn = document.getElementById('first-class-plus-btn');
-firstClassPlusBtn.addEventListener('click',()=> plusBtnHandler("first-class-count"));
-
-const firstClassMinusBtn = document.getElementById('first-class-minus-btn');
-firstClassMinusBtn.addEventListener('click', function(){
-    const firstClassCount = document.getElementById('first-class-count');
-    let countNumber = parseInt(firstClassCount.value);
-    countNumber--;
-    document.getElementById('first-class-count').value = countNumber; 
-});
-
-const economyPlusBtn = document.getElementById('economy-plus-btn');
-economyPlusBtn.addEventListener('click', ()=> plusBtnHandler('economy-count'))
-
-function plusBtnHandler(id){
-    const count = document.getElementById(id);
-    let countNumber = parseInt(count.value);
-    countNumber++;
-    document.getElementById(id).value = countNumber;
+function updateCount(ticketClass, isIncrease){
+    const classCount = document.getElementById(ticketClass + '-count');
+    let countNumber = parseInt(classCount.value);
+    if(isIncrease === true){
+        countNumber++;
+    }
+    if(isIncrease === false && countNumber > 0){
+        countNumber--;
+    }
+    document.getElementById(ticketClass + '-count').value = countNumber; 
 }
