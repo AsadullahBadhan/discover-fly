84cf690b69e622f948af91faf4e570b6dbd6ecb9
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-09-24 02:05:40 +0600

    checkout section added

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-function updateCount(ticketClass, isIncrease){
+function updateCount(ticketClass, isIncrease) {
     let countNumber = toNumber(ticketClass + '-count');
-    if(isIncrease === true){
+    if (isIncrease === true) {
         countNumber++;
     }
-    if(isIncrease === false && countNumber > 0){
+    if (isIncrease === false && countNumber > 0) {
         countNumber--;
     }
     document.getElementById(ticketClass + '-count').value = countNumber;
     calculatePrice();
 }
 
-function toNumber(id){
+function toNumber(id) {
     const element = document.getElementById(id);
     let number = parseInt(element.value);
     return number;
 }
 
-function calculatePrice(){
-    let firstClassPrice = toNumber('first-class-count') * 150;
-    let economyPrice = toNumber('economy-count') * 100;
+function calculatePrice() {
+    let firstClassCount = toNumber('first-class-count');
+    let economyCount = toNumber('economy-count');
+    const firstClassPrice = firstClassCount * 150;
+    const economyPrice = economyCount * 100;
     let subtotal = firstClassPrice + economyPrice;
     let vat = subtotal * 0.1;
     let grandTotal = vat + subtotal;
     document.getElementById('subtotal').innerText = subtotal;
     document.getElementById('vat').innerText = vat;
     document.getElementById('total').innerText = grandTotal;
+    document.getElementById('totalFirstClass').innerText = firstClassCount;
+    document.getElementById('totalEconomy').innerText = economyCount;
+    document.getElementById('total-price').innerText = grandTotal;
 }
+
+const bookingBtn = document.getElementById('booking-btn');
+const bookingSection = document.getElementById('booking-section');
+const checkOut = document.getElementById('check-out');
+bookingBtn.addEventListener('click', () => {
+    bookingSection.style.display = 'none';
+    checkOut.style.display = 'block';
+})
